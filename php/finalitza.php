c39f68bf1bde7510588593b3eb2de373912b4e39
«a24biedommar» <a24biedommar@inspedralbes.cat~>
2025-09-25 09:06:42 +0200

    afegit linies per saber on peta

--- php/finalitza.php
+++ php/finalitza.php
@@ -3,6 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+var_dump($_SESSION['preguntes']);
+var_dump($respostesUsuari);
+
+
 //iniciem la sessió
 session_start();
 

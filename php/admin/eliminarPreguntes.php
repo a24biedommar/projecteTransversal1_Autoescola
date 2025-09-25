344d4b4b570aefe1fa2d14b1c417cc3542e6771e
«a24biedommar» <a24biedommar@inspedralbes.cat~>
2025-09-25 12:08:57 +0200

    errors

--- php/admin/eliminarPreguntes.php
+++ php/admin/eliminarPreguntes.php
@@ -1,4 +1,11 @@
 <?php
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 header('Content-Type: application/json');
 
 $docu = json_decode(file_get_contents('php://input'), true);

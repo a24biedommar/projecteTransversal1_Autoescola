546466ff9ef00776014661c1a81c96591978e4b6
a24biedommar <a24biedommar@inspedralbes.cat>
2025-09-25 20:39:26 +0200

    modificat per saber on peta

--- php/admin/crearPreguntes.php
+++ php/admin/crearPreguntes.php
@@ -3,7 +3,6 @@ header('Content-Type: application/json');
 
 $docu = json_decode(file_get_contents('php://input'), true);
 
-//extreiem les daddes del fitxer json que ens envia lescript
 $preguntaText = $docu['pregunta'];
 $respostes = $docu['respostes'];
 $correctaIndex = $docu['correcta'];
@@ -14,11 +13,11 @@ $username = "a24biedommar_Projecte0";
 $password = "J7CqPQhC|Gwb%=%@";
 $dbname = "a24biedommar_Projecte0";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname) or die("Error de connexió: " . mysqli_connect_error());
 
 // Inserim les dades a la taula preguntes
 $sqlPregunta = "INSERT INTO PREGUNTES (PREGUNTA, LINK_IMATGE) VALUES ('$preguntaText', '$imatge')";
-$conn->query($sqlPregunta);
+$conn->query($sqlPregunta) or die("Error en la consulta de pregunta: " . mysqli_error($conn));
 $idPregunta = $conn->insert_id;
 
 // Inserim les respostes a la taula 'RESPOSTES' amb el bucle foreach
@@ -35,7 +34,7 @@ foreach ($respostes as $index => $resposta) {
     $sqlResposta = "INSERT INTO RESPOSTES (ID_PREGUNTA, RESPOSTA, CORRECTA) VALUES ('$idPregunta', '$resposta', '$isCorrecta')";
     
     //executem la query
-    $conn->query($sqlResposta);
+    $conn->query($sqlResposta) or die("Error en la consulta de resposta: " . mysqli_error($conn));
 }
 
 echo json_encode(['success' => true, 'message' => 'Pregunta i respostes creades correctament']);

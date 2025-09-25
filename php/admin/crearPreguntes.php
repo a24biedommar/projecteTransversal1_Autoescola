8bcb62cc6e45024d028cb4ffe34fdce8a30b71fb
a24biedommar <a24biedommar@inspedralbes.cat>
2025-09-25 20:51:12 +0200

    modificat per fer les querys

--- php/admin/crearPreguntes.php
+++ php/admin/crearPreguntes.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 header('Content-Type: application/json');
 
 $docu = json_decode(file_get_contents('php://input'), true);
@@ -16,28 +13,19 @@ $username = "a24biedommar_Projecte0";
 $password = "J7CqPQhC|Gwb%=%@";
 $dbname = "a24biedommar_Projecte0";
 
-$conn = new mysqli($servername, $username, $password, $dbname) or die("Error de connexió: " . mysqli_connect_error());
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Inserim les dades a la taula preguntes
 $sqlPregunta = "INSERT INTO PREGUNTES (PREGUNTA, LINK_IMATGE) VALUES ('$preguntaText', '$imatge')";
-$conn->query($sqlPregunta) or die("Error en la consulta de pregunta: " . mysqli_error($conn));
+$conn->query($sqlPregunta);
 $idPregunta = $conn->insert_id;
 
-// Inserim les respostes a la taula 'RESPOSTES' amb el bucle foreach
 foreach ($respostes as $index => $resposta) {
-    // Declarem lla variable per saber si es correcta o no
     $isCorrecta = 0;
-    
-    // utilitzem el if per saber si es correcta o no
     if ($index == $correctaIndex) {
         $isCorrecta = 1;
     }
-    
-    // fem la query 
     $sqlResposta = "INSERT INTO RESPOSTES (ID_PREGUNTA, RESPOSTA, CORRECTA) VALUES ('$idPregunta', '$resposta', '$isCorrecta')";
-    
-    //executem la query
-    $conn->query($sqlResposta) or die("Error en la consulta de resposta: " . mysqli_error($conn));
+    $conn->query($sqlResposta);
 }
 
 echo json_encode(['success' => true, 'message' => 'Pregunta i respostes creades correctament']);

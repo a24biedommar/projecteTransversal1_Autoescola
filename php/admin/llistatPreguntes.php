2351e3fbbf606bd54fa318179ec4df35a38370e3
a24biedommar <a24biedommar@inspedralbes.cat>
2025-10-03 10:52:36 +0200

    pujat manualment els fitxers.php
    Anterirment he fet un mere i la he liat pr aixo pujo els fitxers manualment

---
+++ php/admin/llistatPreguntes.php
@@ -0,0 +1,44 @@
+<?php
+// Iniciem sessió
+session_start();
+
+// Inclou el fitxer de connexió
+require_once '../connexio.php';
+
+// Agafem totes les preguntes en ordre d'ID
+$sqlPreguntes = "SELECT * FROM PREGUNTES ORDER BY ID_PREGUNTA";
+$result = $conn->query($sqlPreguntes);
+
+// Declarem l'array on guardarem les preguntes i respostes juntes
+$preguntes = [];
+
+while ($row = $result->fetch_assoc()) {
+    // Agafem l'id de la pregunta actual
+    $idPregunta = $row['ID_PREGUNTA'];
+
+    // Agafem totes les respostes de la pregunta actual
+    $sqlRespostes = "SELECT ID_RESPOSTA, RESPOSTA FROM RESPOSTES WHERE ID_PREGUNTA = $idPregunta ORDER BY ID_RESPOSTA";
+    $resResult = $conn->query($sqlRespostes);
+
+    // Declarem l'array on guardarem les respostes de la pregunta actual
+    $respostes = [];
+    while ($r = $resResult->fetch_assoc()) {
+        $respostes[] = [
+            'id' => $r['ID_RESPOSTA'],
+            'resposta' => $r['RESPOSTA']
+        ];
+    }
+
+    // Afegim la pregunta i les seves respostes a l'array principal (on guardem les preguntes i respostes juntes)
+    $preguntes[] = [
+        'id' => $idPregunta,
+        'pregunta' => $row['PREGUNTA'],
+        'respostes' => $respostes,
+        'imatge' => $row['LINK_IMATGE']
+    ];
+}
+
+// Retornem JSON
+header('Content-Type: application/json');
+echo json_encode(['preguntes' => $preguntes]);
+?>
---
+++ php/finalitza.php
@@ -0,0 +1,44 @@
+<?php 
+
+//iniciem la sessió
+session_start();
+
+// Inclou el fitxer de connexió
+require_once 'connexio.php';
+
+// decodifiquem el fitxer json que ha generat el script de index.html (enviat per petició http)
+$json_data = file_get_contents('php://input');
+$respostesUsuari = json_decode($json_data, true);
+
+//creem les variables seguents:
+$preguntes = $_SESSION['preguntes']; //agafem les preguntes que s'han mostrat a index.html
+$puntuacio = 0; //inicialitzem la puntuació del usuari
+
+// Recorrem les respostes de l'usuari i les comparemm amb les correctes
+foreach ($preguntes as $index => $idPregunta) {
+    // Agafem la resposta de l'usuari per a la pregunta actual
+    $respostaUsuari = $respostesUsuari[$index];
+
+    //Fem la consulta select per agafar la resposta correcta de la BD
+    $sqlCorrecta = "SELECT ID_RESPOSTA FROM RESPOSTES 
+                    WHERE ID_PREGUNTA = $idPregunta AND CORRECTA = 1";
+    $res = $conn->query($sqlCorrecta);
+    $row = $res->fetch_assoc();
+
+    if ($respostaUsuari == $row['ID_RESPOSTA']) {
+        $puntuacio++;
+    }
+}
+
+//Guardem en una variable $resultat el total de preguntes i les pregutnes correctes de l'usuari
+$resultat = [
+    "total" => count($preguntes),
+    "correctes" => $puntuacio
+];
+
+//Eliminem la sessió per poder començar una nova partida
+session_destroy();
+
+//Enviem el fitxer de les respostes de l'usuari en format .json
+header('Content-Type: application/json');
+echo json_encode($resultat);

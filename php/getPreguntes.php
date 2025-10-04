1b6d378300553052c58819952132ab109cf50ed3
biel.acceleralia <biel.dominguez@acceleralia.com>
2025-10-04 13:50:26 +0200

    modificat el getPreguntes perque sigui mes optim (no fa order by rand si no que fa un shufle de les preguntes gracies al codi php enlloc de fer la sentencia select fent que les desordeni ella)

--- php/getPreguntes.php
+++ php/getPreguntes.php
@@ -9,36 +9,30 @@ $_SESSION['index'] = 0;
 $_SESSION['puntuacio'] = 0;
 $_SESSION['preguntes'] = [];
 
-// Consultem totes les preguntes de la base de dades i les barregem aleatòriament
-$sqlPreguntes = "SELECT * FROM PREGUNTES ORDER BY RAND()"; 
+//fem un select per obtenir totes les preguntes de la base de dades.
+$sqlPreguntes = "SELECT * FROM PREGUNTES"; 
 $result = $conn->query($sqlPreguntes);
 
-// Inicialitzem l'array on guardarem les preguntes seleccionades
+//guardem totes les preguntes de la consulta anterior en un array
 $preguntesSeleccionades = [];
-
-// Recorrem cada fila del resultat($result) i l'afegim a l'array $preguntesSeleccionades
 while ($row = $result->fetch_assoc()) {
     $preguntesSeleccionades[] = $row;
 }
 
-//declarem la variable (array) on guardarem les preguntes i respostes sense correctIndex
-$preguntesNoCorrect = [];
+//fem un suffle per barregar l'array anterior
+shuffle($preguntesSeleccionades);
 
-// per cada pregunta seleccionada, busquem les respostes
+$preguntesNoCorrect = [];
+$stmtRespostes = $conn->prepare("SELECT ID_RESPOSTA, RESPOSTA FROM RESPOSTES WHERE ID_PREGUNTA = ? ORDER BY RAND()");
+
 foreach($preguntesSeleccionades as $pregunta){
-    // guardem l'id de la pregunta actual
     $id = $pregunta['ID_PREGUNTA'];
 
-    //Per evitar errors de vulnerabilitat fem servir prepared statements
-    
-    $stmt = $conn->prepare("SELECT ID_RESPOSTA, RESPOSTA FROM RESPOSTES WHERE ID_PREGUNTA = ? ORDER BY RAND()");
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-    $resResult = $stmt->get_result();
+    $stmtRespostes->bind_param("i", $id);
+    $stmtRespostes->execute();
+    $resResult = $stmtRespostes->get_result();
 
     $respostes = [];
-
-    //fem un bucle per guardar les respostes de la pregunta actual
     while($r = $resResult->fetch_assoc()){
         $respostes[] = [
             'id' => $r['ID_RESPOSTA'],
@@ -46,7 +40,6 @@ foreach($preguntesSeleccionades as $pregunta){
         ];
     }
 
-    // Afegim la pregunta completa (tots els seus atributs) a l'array final
     $preguntesNoCorrect[] = [
         'id' => $pregunta['ID_PREGUNTA'],
         'pregunta' => $pregunta['PREGUNTA'],
@@ -54,9 +47,10 @@ foreach($preguntesSeleccionades as $pregunta){
         'imatge' => $pregunta['LINK_IMATGE']
     ];
 
-    // Guardem només l'ID de la pregunta a la sessió per a futures validacions
     $_SESSION['preguntes'][] = $pregunta['ID_PREGUNTA'];
 }
+
+$stmtRespostes->close();
 
 // Creem el fitxer JSON amb les preguntes que mostrarem a l'usuari
 header('Content-Type: application/json');
